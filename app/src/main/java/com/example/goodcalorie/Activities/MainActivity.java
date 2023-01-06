df1480508614e424c1e7504a33cad0b364279534
Kacper Skrzyniarz <kacper.skr11@gmail.com>
2023-01-06 20:12:10 +0100

    add intent to button

--- app/src/main/java/com/example/goodcalorie/Activities/MainActivity.java
+++ app/src/main/java/com/example/goodcalorie/Activities/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.goodcalorie;
+package com.example.goodcalorie.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.goodcalorie.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity
@@ -21,6 +23,10 @@ public class MainActivity extends AppCompatActivity
         recyclerView = findViewById(R.id.recyclerView);
         addButton = findViewById(R.id.addButton);
 
-        
+        addButton.setOnClickListener(view ->
+        {
+            Intent intent = new Intent(this, AddActivity.class);
+            startActivity(intent);
+        });
     }
 }
