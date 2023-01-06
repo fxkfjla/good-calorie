91d540beca2dc344c8d5d25b35bdee25cf414728
Kacper Skrzyniarz <kacper.skr11@gmail.com>
2023-01-06 20:09:57 +0100

    add recyclerview dependency

---
+++ app/src/main/java/com/example/goodcalorie/Activities/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.goodcalorie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity
+{
+    RecyclerView recyclerView;
+    FloatingActionButton addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        addButton = findViewById(R.id.addButton);
+
+        
+    }
+}
