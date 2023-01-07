658a188f460bf4c0c54576425a5eb17e4b60d21e
Kacper Skrzyniarz <kacper.skr11@gmail.com>
2023-01-07 20:53:46 +0100

    add life cycle functions

--- app/src/main/java/com/example/goodcalorie/Activities/MainActivity.java
+++ app/src/main/java/com/example/goodcalorie/Activities/MainActivity.java
@@ -37,6 +37,42 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
